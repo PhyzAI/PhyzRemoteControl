164d3856a0af7f24390934a9de99cb0f59cb50a5
PhyzAI <155505908+PhyzAI@users.noreply.github.com>
2025-01-02 14:16:34 -0800

    Tweaked left-arm range

--- RemoteControlPhyz_withVision.py
+++ RemoteControlPhyz_withVision.py
@@ -109,7 +109,7 @@ head_y_range = (735*4, 936*4, 1136*4) # head up / down
 #head_tilt_range = (1400*4, 1450*4, 1500*4)   # old.  Not sure why it changed. 
 head_tilt_range = (1237*4, 1337*4, 1437*4) 
 arm_right_range = (1536*4, 2608*4, 2608*4) 
-arm_left_range = (1184*4, 1184*4, 1744*4)
+arm_left_range = (944*4, 2000*4, 2000*4)
         
 
 
